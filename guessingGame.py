2d72698bbd7db7e6e5f21576b59443d7ab1dc3c0
UwU-UwU-UwU-UwU-UwU <72279324+UwU-UwU-UwU-UwU-UwU@users.noreply.github.com>
2021-12-28 17:02:29 -0500

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,64 @@
+import random
+number=random.randint(1,1)
+print("Number guessing game")
+#def attempt():
+    #print("Guess a number from 1 to 9")
+    #guess1=input("Enter your guess: ")
+    #guess=int(guess1)
+    #if (guess==number):
+        #print("You Win!")
+        #quit()
+    #elif (guess>number):
+        #print("Your guess is too high.")
+    #else:
+        #print("Your guess is too low.")
+print("Guess a number from 1 to 9")
+guess1=input("Enter your guess: ")
+guess=int(guess1)
+if (guess==number):
+    print("You Win!")
+    quit()
+elif (guess>number):
+    print("Your guess is too high.")
+else:
+    print("Your guess is too low.")
+print("Guess a number from 1 to 9")
+guess1=input("Enter your guess: ")
+guess=int(guess1)
+if (guess==number):
+    print("You Win!")
+    quit()
+elif (guess>number):
+    print("Your guess is too high.")
+else:
+    print("Your guess is too low.")
+print("Guess a number from 1 to 9")
+guess1=input("Enter your guess: ")
+guess=int(guess1)
+if (guess==number):
+    print("You Win!")
+    quit()
+elif (guess>number):
+    print("Your guess is too high.")
+else:
+    print("Your guess is too low.")
+print("Guess a number from 1 to 9")
+guess1=input("Enter your guess: ")
+guess=int(guess1)
+if (guess==number):
+    print("You Win!")
+    quit()
+elif (guess>number):
+    print("Your guess is too high.")
+else:
+    print("Your guess is too low.")
+print("Guess a number from 1 to 9")
+guess1=input("Enter your guess: ")
+guess=int(guess1)
+if (guess==number):
+    print("You Win!")
+    quit()
+elif (guess>number):
+    print("You lose! The number is: ",number)
+else:
+    print("You lose! The number is: ",number)
